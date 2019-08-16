69913e40c63769b748f2eabac76f4fdc4f569f89
Simon Seto <sseto@westvancouver.ca>
2019-08-16 15:23:07 -0700

    add filter_var and preg_match for errors

--- add.php
+++ add.php
@@ -1,4 +1,5 @@
 <?php 
+//isset is check if certian variable/value has been set
 
 	// if(isset($_GET['submit'])){
 	// 	echo $_GET['email'];
@@ -7,11 +8,42 @@
 	// }
 
 	// Using POST is more secure, because once submitted, you won't see your order in the URL
+//htmlspecialchars is creating special characters like angle brackets will be render into HTML entities
 	if(isset($_POST['submit'])){
-		echo $_POST['email'];
-		echo $_POST['title'];
-		echo $_POST['ingredients'];
-	}
+		// echo htmlspecialchars($_POST['email']);
+		// echo htmlspecialchars($_POST['title']);
+		// echo htmlspecialchars($_POST['ingredients']);
+
+		// check email
+		if(empty($_POST['email'])){
+			echo 'An email is required <br>';
+		} else {
+			$email = $_POST['email'];
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				echo 'Email must be a valid email address';
+			}
+		}
+
+		// check title
+		if(empty($_POST['title'])){
+			echo 'A title is required <br>';
+		} else {
+			$title = $_POST['title'];
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				echo 'Title must be letters and spaces only';
+			}
+		}
+
+		// check ingredients
+		if(empty($_POST['ingredients'])){
+			echo 'At least one ingredient is required <br>';
+		} else {
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				echo 'Ingredients must be a comma separated list';
+			}
+		}
+	} // end of POST check
 		
 ?>
 
