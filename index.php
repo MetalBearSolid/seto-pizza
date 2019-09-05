922eb8f7552c336474fb496572eb54ef89cfd999
Simon Seto <sseto@westvancouver.ca>
2019-09-05 09:29:49 -0700

    connecting to DB

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php 
+
+	// connect to database
+	$conn = mysqli_connect('localhost', 'simon', 'Daemonhobo123', 'seto_pizza');
+
+	// check connection
+	if(!$conn){
+		echo 'Connection error: ' . mysqli_connect_error();
+	}
+
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+	// make query & get results
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	
+	// free result from memory
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+
+	print_r($pizzas);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include('templates/header.php') ?>
