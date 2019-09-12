03a7ca22bcad12d0b9c17cb57b5e5f5ae72c3165
Simon Seto <sseto@westvancouver.ca>
2019-09-12 15:52:48 -0700

    printing out our pizzas into the website

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	}
 
 	// write query for all pizzas
-	$sql = 'SELECT title, ingredients, id FROM pizzas';
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 	// make query & get results
 	$result = mysqli_query($conn, $sql);
@@ -24,12 +24,46 @@
 	// close connection
 	mysqli_close($conn);
 
-	print_r($pizzas);
+	// this will create a LIST of ingredients
+	explode(',', $pizzas[0]['ingredients']);
+
+	// print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <?php include('templates/header.php') ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+	<div class="row">
+		<?php foreach($pizzas as $pizza): ?>
+			<div class="col s6 md3">
+				<div class="card z-depth-0">
+					<div class="card-content center">
+						<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+						<ul>
+							<!-- Create each ingredient as $ing to list it as a list-->
+							<?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
+								<li><?php echo htmlspecialchars($ing) ?></li>
+							<?php endforeach ?>
+						</ul>
+					</div>
+					<div class="card-action right-align">
+						<a href="#" class="brand-text">more info</a>
+					</div>
+				</div>
+			</div>
+		<?php endforeach ?>
+
+		<?php if(count($pizzas) >= 3): ?>
+			<p>there are 2 or more pizzas</p>
+		<?php else: ?>
+			<p>there are less than 3 pizzas</p>
+		<?php endif; ?>
+	</div>
+</div>
 
 <?php include('templates/footer.php') ?>
 	
