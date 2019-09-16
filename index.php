491a5e1c3a5e402c51fc55eaccc047c689ca91bc
Simon Seto <sseto@westvancouver.ca>
2019-09-16 14:10:25 -0700

    add delete functionality and style the pizza a bit

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ include('config/db_connect.php');
 		<?php foreach($pizzas as $pizza): ?>
 			<div class="col s6 md3">
 				<div class="card z-depth-0">
+					<img src="img/pizza.svg" class="pizza" alt="pizzas">
 					<div class="card-content center">
 						<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
 						<ul>
@@ -50,12 +51,6 @@ include('config/db_connect.php');
 				</div>
 			</div>
 		<?php endforeach ?>
-
-		<?php if(count($pizzas) >= 3): ?>
-			<p>there are 2 or more pizzas</p>
-		<?php else: ?>
-			<p>there are less than 3 pizzas</p>
-		<?php endif; ?>
 	</div>
 </div>
 
